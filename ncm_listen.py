38d1e8e557a97aca68231cedcec9d2bd53d21103
lixvbnet <lixvbnet@gmail.com>
2022-12-19 17:42:50 +0800

    fix actions

--- ncm_listen.py
+++ ncm_listen.py
@@ -70,37 +70,41 @@ listenCount = 0
 
 # random choose playlists
 for p in random.sample(playlists, len(playlists)):
-    pInfo = playlist.GetPlaylistInfo(p['id'])
-    pList = pInfo['playlist']
-    print(f"\nCurrent playlist: [{pList['name']}]")
-    songIds = list(map(lambda item: item['id'], pList['trackIds']))
-    songList = track.GetTrackDetail(songIds)['songs']
-    print(f"{len(songList)} songs")
-    print(f"------------------------------------------")
+    try:
+        pInfo = playlist.GetPlaylistInfo(p['id'])
+        pList = pInfo['playlist']
+        print(f"\nCurrent playlist: [{pList['name']}]")
+        songIds = list(map(lambda item: item['id'], pList['trackIds']))
+        songList = track.GetTrackDetail(songIds)['songs']
+        print(f"{len(songList)} songs")
+        print(f"------------------------------------------")
 
-    exit_flag = False
-    songs = []
-    # random choose songs
-    max_listen_per_playlist = random.randint(50, 70)
-    for i, song in enumerate(random.sample(songList, len(songList))[:max_listen_per_playlist]):
-        if count >= MAX_LISTEN:
-            exit_flag = True
+        exit_flag = False
+        songs = []
+        # random choose songs
+        max_listen_per_playlist = random.randint(50, 70)
+        for i, song in enumerate(random.sample(songList, len(songList))[:max_listen_per_playlist]):
+            if count >= MAX_LISTEN:
+                exit_flag = True
+                break
+            if DEBUG: print(f"Adding '{song['name']}', id={song['id']}, album: {song['al']['name']} ({song['al']['id']})")
+            songs.append(Song(songId=song['id'], sourceId=pList['id'], listenTime=song['dt']))
+            count += 1
+            if DEBUG: print(f"count = {count}")
+
+        # listen songs
+        if songs:
+            print(f"Listening {len(songs)} songs from playlist '{pList['name']}'")
+            listen(songs)
+            listenCount += len(songs)
+            sec = 1
+            print(f"Sleeping {sec} seconds...")
+            time.sleep(sec)
+
+        if exit_flag:
             break
-        if DEBUG: print(f"Adding '{song['name']}', id={song['id']}, album: {song['al']['name']} ({song['al']['id']})")
-        songs.append(Song(songId=song['id'], sourceId=pList['id'], listenTime=song['dt']))
-        count += 1
-        if DEBUG: print(f"count = {count}")
-
-    # listen songs
-    if songs:
-        print(f"Listening {len(songs)} songs from playlist '{pList['name']}'")
-        listen(songs)
-        listenCount += len(songs)
-        sec = 1
-        print(f"Sleeping {sec} seconds...")
-        time.sleep(sec)
-
-    if exit_flag:
-        break
+    except Exception as ex:
+        print("Error:", ex)
+        continue
 
 print(f"\n完成！一共听歌{listenCount}首。")
